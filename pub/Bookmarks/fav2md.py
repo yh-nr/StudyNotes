76a60123fa291b91502fcce59166f91b488b940f
yh-nr <ynaru911@gmail.com>
2024-03-09 23:48:08 +0900

    0309

--- pub/Bookmarks/fav2md.py
+++ pub/Bookmarks/fav2md.py
@@ -5,16 +5,17 @@ from bs4 import BeautifulSoup, Comment
 
 # 半角スペースの連続後に#があるパターンにマッチし、置換する関数
 def replace_spaces(match):
-    # スペースの連続を取得
     spaces = match.group(0)
-    # スペースの数を4で割り、その商だけ#に置き換える
-    replacement = "\n" + "#" * (len(spaces) // 4)
+    replacement = "\n" + "#" * (len(spaces) - 3)
     return replacement
 
 
 def html_to_markdown(html_file_path, output_md_path, input_md_path):
     with open(html_file_path, "r", encoding="utf-8") as file:
         html_content = file.read()
+
+    lines = html_content.split("\n")
+    html_content = "\n".join(lines[1:])
 
     html_content = (
         html_content.replace("    ", "$ ").replace("$ $", "$$").replace("$ $", "$$")
@@ -27,8 +28,8 @@ def html_to_markdown(html_file_path, output_md_path, input_md_path):
         comment.extract()
 
     # TITLE、H1タグを取り除く
-    for tag in soup.find_all("TITLE"):
-        tag.decompose()
+    for tag in soup.find_all(["title", "h1"]):
+        tag.extract()
 
     # DT、DL、Pタグの中身を保持してタグ自体は取り除く
     for tag in soup.find_all(["dt", "dl", "p"]):
@@ -40,38 +41,38 @@ def html_to_markdown(html_file_path, output_md_path, input_md_path):
 
     for element in soup.contents:
         if element.name == "h3":
-            markdown_content += "#" + " " + element.get_text().strip() + "\n"
+            markdown_content += "#" + " " + element.get_text().strip()
         elif element.name == "a":
             # リンクをマークダウン形式に変換
             link_text = element.get_text()
             href = element.get("href", "")
             icon_html = ""
             if element.has_attr("icon"):
-                print("iconあります")
                 # icon要素がある場合、HTMLイメージタグを使用して行頭に表示
                 icon_src = element["icon"]
             else:
                 icon_src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAACC0lEQVR4nO2Wy0pcQRCGv8WYTcaJ2SfgwnGEbGJGs84beAHX+g7esnQhCVmFiBshJuYZsorjKL6CkJgIIoIukxAVzWYcKfgHGrH7dJ9RUPCHhsOp6uq/q7oucI87hgowC6wB28CJln3XgBmg9yYOHgDqQDNyGcHqdRzcASwC5zL8G1gGhuSNh8ADDwnbswAU8h7+GFiXsVNgHih5dLO80ZXn5nUZOAQGr9B5lhCSTXkqGovaeAA88ejMJRCw9SHlwZ3L7aGH9D2RQAN4EUOgrg0Wcx+aOZelahAV57U/Ig2xISmHjLyW0kfyI4vAdGhzTUqW5yFY/n8G9oAVYBzojvTCasjwjpR6Ajp9wFZi7JFN+/4ZInAkpU6PfEK1P3SYeWEM2AWWnEwqSn4UQ6Dokcfc9pVnb0nyfyECv6TU2waBjUDomuqcmY9wuA0CbtxdjOr/txCBGSl98shn2/DCF8kmYwrRn0AhmsvxDqwb/o0pRKh9muIb4hDj/ncx7m+hqmZ0psaUhcuHW4Fy8VK2rBk9JxILEe34cuo21SGtSrbwVPOEyd6TgIITikPdwocl6Z1oSGmhH9h3umDyaNblkDAXvvWMVlXpTDj7LOb/ndqfPJK1UNAk03Cyw2I8osJS1OrTvxXntTfk9txDqQtz7deEIlSLnX5SUVY/X1VJPdb6oRSbyuik97h9uAA+3jTLypXd7wAAAABJRU5ErkJggg=="
-            icon_html = f"![with: 16px;]({icon_src})"
-            markdown_content += f"- [{icon_html}{link_text}]({href})\n"
+            icon_html = f"![width:24px]({icon_src})"
+            markdown_content += f"- [{icon_html}{link_text}]({href})"
         elif element.name is None and element.string is not None:
-            markdown_content += element.string.strip()
-        elif element.name not in ["h3", "a"]:
-            markdown_content += element.get_text(separator="\n", strip=True) + "\n"
-
-        markdown_content = markdown_content.replace("$", "    ")
+            markdown_content += "".join(element.string)
 
         # 正規表現で置換
-        markdown_content = re.sub(r"\n {4,}(?=#)", replace_spaces, markdown_content)
-        markdown_content = re.sub(r"\n {4}", "\n", markdown_content)
+        markdown_content2 = re.sub(r"\n\$+ #", replace_spaces, markdown_content)
+        markdown_content3 = markdown_content2.replace("$", "")
+
+    # 文字列を改行で分割してリストにする
+    lines = markdown_content3.split("\n")
+    non_blank_lines = [line for line in lines if line.strip()]
+    markdown_content4 = "\n".join(non_blank_lines)
 
     # MDファイルとして出力
     with open(output_md_path, "w", encoding="utf-8") as md_file:
-        md_file.write(markdown_content)
+        md_file.write(markdown_content4)
 
 
 # 使用例
 html_file_path = "input.html"  # 入力HTMLファイルのパス
-output_md_path = "output.md"  # 出力するMarkdownファイルのパス
-input_md_path = "input.md"
+output_md_path = "bookmarks.md"  # 出力するMarkdownファイルのパス
+input_md_path = "inputmd.txt"
 html_to_markdown(html_file_path, output_md_path, input_md_path)
